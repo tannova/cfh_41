1bb40dd55356ea60ed7200bae5e9f3bb9df9c165
tan.nguyen3 <tan.nguyen3@mservice.com.vn>
2020-07-26 17:38:01 +0700

    [CFH-41] init project example

---
+++ demoweb/src/main/java/demo/cfh/demoweb/DemowebApplication.java
@@ -0,0 +1,16 @@
+package demo.cfh.demoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@Component
+public class DemowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebApplication.class, args);
+
+	}
+
+}
---
+++ demoweb/src/test/java/demo/cfh/demoweb/DemowebApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.cfh.demoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
